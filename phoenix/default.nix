0050395175d2352fa6abf173f968a1bccdce772d
creator54 <hi.creator54@gmail.com>
2024-06-25 01:53:51 +0530

    `feat(phoenix): add git to system packages`

--- phoenix/default.nix
+++ phoenix/default.nix
@@ -1,6 +1,10 @@
+{ pkgs, ... }:
 {
   imports = [
     ./../server/configuration.nix
     ./../server/hardware-configuration.nix
   ];
+
+  #add on top of ./../server/configuration.nix
+  environment.systemPackages = with pkgs; [ git ];
 }
