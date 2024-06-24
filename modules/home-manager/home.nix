9383436c65f97cd318850f84e831d69daa1b176e
creator54 <hi.creator54@gmail.com>
2024-06-25 03:19:04 +0530

    feat(home-manager): enable config backup by default with .bkp extension

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -1,15 +1,23 @@
 { ... }:
 {
-  home-manager.users."creator54" = { ... }: {
-    imports = [
-      ./pkgs/general.nix
-      ./configs/symlinks.nix
-    ];
+  home-manager = {
+    users."creator54" = { ... }: {
+      imports = [
+        ./pkgs/general.nix
+        ./configs/symlinks.nix
+      ];
 
-    home = {
-      username = "creator54";
-      homeDirectory = "/home/creator54";
-      stateVersion = "23.11";
+      home = {
+        username = "creator54";
+        homeDirectory = "/home/creator54";
+        stateVersion = "23.11";
+      };
     };
+
+    # do home-manager switch -b by default
+    # ref: https://nix-community.github.io/home-manager/nixos-options.xhtml#nixos-opt-home-manager.backupFileExtension
+    # ref: https://discourse.nixos.org/t/way-to-automatically-override-home-manager-collisions/33038
+    # ref: https://www.reddit.com/r/NixOS/comments/1d3f15l/homemanager_wants_something_strange/
+    backupFileExtension = "bkp";
   };
 }
