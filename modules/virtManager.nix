73894c24265b98accfe78a2715d764f1ee765309
creator54 <hi.creator54@gmail.com>
2024-12-21 20:13:04 +0530

    feat(system): enable AMD GPU support and virtmanager in omnix config

--- modules/virtManager.nix
+++ modules/virtManager.nix
@@ -3,5 +3,7 @@
 {
   environment.systemPackages = with pkgs; [ virt-manager virt-viewer quickemu ];
   users.users.creator54.extraGroups = [ "libvirtd" ];
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+  };
 }
