91f53a7da63705005d77c7a1f2039f62ed8374dc
creator54 <hi.creator54@gmail.com>
2024-12-28 23:42:51 +0530

    feat(config): add fusuma touchpad gestures and workspace keybinds
    - Add fusuma package for touchpad gesture support
    - Configure 3-finger swipe for browser navigation
    - Configure 4-finger swipe for workspace switching
    - Add Control+arrow keybinds for workspace navigation
    - Disable mutter tiling keybinds to avoid conflicts
    - Add input group to user permissions for gesture support

---
+++ modules/configs/full/pkgs/fusuma.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  services.fusuma = {
+    enable = true;
+    settings = {
+      threshold = { swipe = 0.1; };
+      interval = { swipe = 0.7; };
+      swipe = {
+        # allow going back/forward in browsers
+        "3" = {
+          right = {
+            command = "xdotool key alt+Left";
+          };
+          left = {
+            command = "xdotool key alt+Right";
+          };
+        };
+        "4" = {
+          right = {
+            # GNOME: Switch to left workspace
+            command = "xdotool key ctrl+Left";
+          };
+          left = {
+            # GNOME: Switch to right workspace
+            command = "xdotool key ctrl+Right";
+          };
+        };
+      };
+    };
+  };
+}
+
