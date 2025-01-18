23fb39d6fdff1d64a942012d698a2c1484f211dd
creator54 <hi.creator54@gmail.com>
2025-01-18 17:13:04 +0530

    revert(configs): remove allowOverwrite setting and restore backupFileExtension
    This reverts the previous attempt to allow overriding impure configurations
    directly as it did not work as expected. Restores backupFileExtension to "bkp"
    with explanatory comments about home-manager behavior.

--- modules/configs/minimal/default.nix
+++ modules/configs/minimal/default.nix
@@ -19,9 +19,10 @@ in
       };
     };
 
-    # Remove backup extension and add overwrite setting
-    extraSpecialArgs = {
-      home.file.allowOverwrite = true;
-    };
+    # do home-manager switch -b by default
+    # ref: https://nix-community.github.io/home-manager/nixos-options.xhtml#nixos-opt-home-manager.backupFileExtension
+    # ref: https://discourse.nixos.org/t/way-to-automatically-override-home-manager-collisions/33038
+    # ref: https://www.reddit.com/r/NixOS/comments/1d3f15l/homemanager_wants_something_strange/
+    backupFileExtension = "bkp";
   };
 }
