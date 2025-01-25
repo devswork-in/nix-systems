d5973273e3a8bfae46fc097ac7c0d43dc63caa33
creator54 <hi.creator54@gmail.com>
2025-01-25 15:16:08 +0530

    refactor(power): extract tlp configuration to separate module

--- modules/local-system/services.nix
+++ modules/local-system/services.nix
@@ -50,21 +50,6 @@
       ''
     ];
 
-    tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 95;
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 10;
-        SOUND_POWER_SAVE_ON_AC = 0;
-        SOUND_POWER_SAVE_ON_BAT = 1;
-        CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-      };
-    };
-
-
     openssh = {
       enable = true;
       settings = {
