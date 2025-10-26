6badb13208f964dc574ae852e8ebee9eb9c98c7a
creator54 <hi.creator54@gmail.com>
2025-10-26 07:56:10 +0530

    perf(services): disable non-essential services at boot to improve boot time

--- modules/addons/services/mysql.nix
+++ modules/addons/services/mysql.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 
 {
+  # MySQL service - disabled at boot to improve boot time
+  # To start when needed: sudo systemctl start mysql
   services.mysql = {
-    enable = true;
+    enable = false;  # Changed to false - start manually when needed
     package = pkgs.mariadb;
   };
 }
--- modules/essential/networking/default.nix
+++ modules/essential/networking/default.nix
@@ -4,4 +4,14 @@
     ./wireguard.nix
     ./hosts.nix
   ];
+
+  # Disable services not needed for typical WiFi-only laptops
+  # These can be enabled in specific configurations if needed
+  services.avahi = {
+    enable = false;  # Not typically needed on WiFi laptops
+  };
+
+  # Disable Bluetooth if not needed (can be enabled in specific configs)
+  # hardware.bluetooth.enable = false;
+  # services.blueman.enable = false;
 }
