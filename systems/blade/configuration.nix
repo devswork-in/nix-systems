b72c7f44314e562dae7f109a04f7f00fc0a485d8
creator54 <hi.creator54@gmail.com>
2025-12-03 02:58:42 +0530

    refactor(nixos): consolidate flake inputs and standardize nix cache configuration

--- systems/blade/configuration.nix
+++ systems/blade/configuration.nix
@@ -9,9 +9,6 @@
     ./hardware.nix
     ./fileSystems.nix
     
-    # Addon modules
-    ../../modules/services/docker
-    ../../modules/services/flatpak.nix
   ];
 
   # System-specific hostname (overrides profile default)
@@ -20,15 +17,5 @@
   # Example: Override sync items for this system
   # services.nix-repo-sync.syncItems = lib.mkForce [ /* ... */ ];
   
-  # System-specific nix caches (extends profile caches)
-  nix.settings.substituters = [
-    "file:///home/${userConfig.user.name}/.nix-cache"
-    "https://cache.iog.io"
-    "https://cache.garnix.io?priority=41"
-  ];
-  
-  nix.settings.trusted-public-keys = [
-    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-  ];
+
 }
