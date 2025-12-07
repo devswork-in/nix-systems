eb7861232ed227e4a1079950fbcc9db1f6b68619
creator54 <hi.creator54@gmail.com>
2025-12-08 01:43:31 +0530

    feat(fish): add editor keybind

--- modules/core/configs/common/fish/functions/fish_user_key_bindings.fish
+++ modules/core/configs/common/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,11 @@
 function fish_user_key_bindings
     # Disable terminal flow control to allow ctrl+s
     stty -ixon 2>/dev/null
-    
+
     bind '!' bind_bang
     bind '$' bind_dollar
     bind \cs 'fish_search_files; commandline -f repaint'
-    bind \cp 'toggle_prompt'
+    bind \cp toggle_prompt
+    bind \ce 'vim .'
 end
+
