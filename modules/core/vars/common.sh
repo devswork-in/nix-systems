b74658294cbf76333e916c38ba5ff713d851cd5f
creator54 <hi.creator54@gmail.com>
2025-12-28 18:46:43 +0530

    Refactor: Explicitly set NIX_CONFIG_DIR in shell configs

--- modules/core/vars/common.sh
+++ modules/core/vars/common.sh
@@ -6,3 +6,15 @@ export VISUAL="nvim"
 
 # Add user paths
 export PATH="$HOME/.local/bin:$HOME/.npm-global/bin:$HOME/.bun/bin:$PATH"
+
+# System Identity (Explicitly set for shell)
+export NIX_CONFIG_DIR="/etc/nixos"
+if [ -z "$NIX_SYSTEM" ]; then
+    export NIX_SYSTEM="$(hostname)"
+fi
+
+# System Identity (Explicitly set for shell)
+export NIX_CONFIG_DIR="/etc/nixos"
+if [ -z "$NIX_SYSTEM" ]; then
+    export NIX_SYSTEM="$(hostname)"
+fi
