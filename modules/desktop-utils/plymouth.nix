5b79f5c0a868567108b4e685d38aae87845c7494
creator54 <hi.creator54@gmail.com>
2026-01-29 04:05:50 +0530

    feat(boot): revert to default BGRT plymouth theme

--- modules/desktop-utils/plymouth.nix
+++ modules/desktop-utils/plymouth.nix
@@ -1,29 +1,6 @@
 { pkgs, ... }:
 
-let
-  blockchain = pkgs.stdenv.mkDerivation {
-    pname = "adi1090x-plymouth";
-    version = "0.0.1";
-    src = builtins.fetchurl {
-      url =
-        "https://github.com/adi1090x/files/raw/master/plymouth-themes/themes/pack_1/blockchain.tar.gz";
-      sha256 = "sha256:1f60nvrk506bqw47g90wzbvn3bp5h1gbi0ll5f3bd6wj77qfk05i";
-    };
-
-    configurePhase = ''
-      mkdir -p $out/share/plymouth/themes/
-    '';
-
-    buildPhase = ''
-      tar -xvzf $src
-    '';
-
-    installPhase = ''
-      cp -r blockchain $out/share/plymouth/themes
-      cat blockchain/blockchain.plymouth | sed  "s@\/usr\/@$out\/@" > $out/share/plymouth/themes/blockchain/blockchain.plymouth
-    '';
-  };
-in {
+{
   boot = {
     kernelParams = [
       "quiet"
@@ -43,10 +20,9 @@ in {
     consoleLogLevel = 0;
     plymouth = {
       enable = true;
-      themePackages = [ blockchain ];
-      theme = "blockchain";
+      # "bgrt" is the default theme that uses the UEFI/BIOS logo
+      theme = "bgrt";
     };
     initrd.verbose = false;
-    # initrd.systemd.enable = true; # Disabled to fix Plymouth race condition
   };
 }
