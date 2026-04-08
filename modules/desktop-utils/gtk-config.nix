701e3b4144eaaca1f61d690aefdca92682c84358
creator54 <hi.creator54@gmail.com>
2026-04-08 23:59:05 +0530

    theme: unify desktop to Gruvbox with aqua window glow

--- modules/desktop-utils/gtk-config.nix
+++ modules/desktop-utils/gtk-config.nix
@@ -1,6 +1,10 @@
-# Centralized GTK configuration module
-{ pkgs, ... }:
+# Centralized GTK configuration module — Gruvbox unified theme
+{ pkgs, config, ... }:
 
+let
+  gruvbox-theme = pkgs.gruvbox-gtk-theme;
+  themeName = "gruvbox-dark";
+in
 {
   gtk = {
     enable = true;
@@ -11,13 +15,14 @@
     };
 
     theme = {
-      name = "palenight";
-      package = pkgs.palenight-theme;
+      name = themeName;
+      package = gruvbox-theme;
     };
 
     cursorTheme = {
-      name = "Numix-Cursor";
-      package = pkgs.numix-cursor-theme;
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
     };
 
     gtk3.extraConfig = { gtk-application-prefer-dark-theme = 1; };
@@ -25,13 +30,20 @@
     gtk4.extraConfig = { gtk-application-prefer-dark-theme = 1; };
   };
 
+  # GTK 4 theme symlinks
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
+
   # Set consistent GTK theme variable
-  home.sessionVariables = { GTK_THEME = "palenight"; };
+  home.sessionVariables = { GTK_THEME = themeName; };
 
   # GNOME interface settings that complement GTK
   dconf.settings = {
     "org/gnome/desktop/interface" = {
-      gtk-theme = "palenight";
+      gtk-theme = themeName;
       color-scheme = "prefer-dark";
       icon-theme = "Papirus-Dark";
     };
