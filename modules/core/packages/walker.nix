384e0a39a42ccef10712ed268058d34079c3e12c
creator54 <hi.creator54@gmail.com>
2026-04-18 11:56:58 +0530

    feat(walker): add config and elephant profiles
    Add default and elephant provider profiles for walker with keybinds.

--- modules/core/packages/walker.nix
+++ modules/core/packages/walker.nix
@@ -7,5 +7,51 @@
 
   programs.walker = {
     enable = true;
+
+    config = {
+      providers.default = [
+        "desktopapplications"
+        "clipboard"
+        "files"
+        "calc"
+        "symbols"
+        "websearch"
+        "runner"
+        "windows"
+        "todo"
+        "bookmarks"
+        "niriactions"
+      ];
+
+      keybinds = {
+        close = ["Escape"];
+        next = ["Down"];
+        previous = ["Up"];
+        left = ["Left"];
+        right = ["Right"];
+        toggle_exact = ["ctrl e"];
+        resume_last_query = ["ctrl r"];
+        quick_activate = ["F1" "F2" "F3" "F4"];
+        page_down = ["Page_Down"];
+        page_up = ["Page_Up"];
+        show_actions = ["alt j"];
+      };
+    };
+
+    elephant = {
+      providers = [
+        "desktopapplications"
+        "files"
+        "clipboard"
+        "calc"
+        "symbols"
+        "websearch"
+        "runner"
+        "windows"
+        "todo"
+        "bookmarks"
+        "niriactions"
+      ];
+    };
   };
 }
