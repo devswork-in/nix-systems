002995096a753d1f681e4681dee4d8623b7ae09c
creator54 <hi.creator54@gmail.com>
2026-04-25 14:48:14 +0530

    feat: add pi key bindings and awrit layout shortcuts

--- modules/core/configs/common/fish/functions/fish_user_key_bindings.fish
+++ modules/core/configs/common/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,28 @@
+function __pi_resume
+    commandline -r ""
+    commandline -f repaint
+    
+    # Convert current path to Pi's session directory format
+    # /home/creator54/loomwork -> --home-creator54-loomwork--
+    set -l cwd (pwd)
+    set -l session_dir (echo $cwd | sed 's|^/||; s|/$||; s|/|-|g')
+    set -l session_dir "--$session_dir--"
+    
+    # Check if session directory exists with any .jsonl files
+    if test -d ~/.pi/agent/sessions/$session_dir
+        and count ~/.pi/agent/sessions/$session_dir/*.jsonl >/dev/null 2>&1
+        pi -r
+    else
+        pi
+    end
+end
+
+function __pi_new
+    commandline -r ""
+    commandline -f repaint
+    pi
+end
+
 function fish_user_key_bindings
     # Disable terminal flow control to allow ctrl+s
     stty -ixon 2>/dev/null
@@ -6,6 +31,7 @@ function fish_user_key_bindings
     bind '$' bind_dollar
     bind \cs 'fish_search_files; commandline -f repaint'
     bind \cp toggle_prompt
+    bind \ck __pi_resume
+    bind \cn __pi_new
     bind \ce 'vim .'
 end
-
