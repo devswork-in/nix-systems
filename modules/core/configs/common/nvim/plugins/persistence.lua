1da2afe84744817e29120c37461fb1bb428f5ffd
creator54 <hi.creator54@gmail.com>
2026-05-16 13:47:56 +0530

    feat(nvim): add auto-session persistence
    - Add persistence.nvim for session management\n- Configure auto-restore when opening nvim without arguments

---
+++ modules/core/configs/common/nvim/plugins/persistence.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file is opened
+    opts = {
+      -- options
+    },
+    -- Automatically restore session if opening nvim without arguments
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        group = vim.api.nvim_create_augroup("PersistenceAutoRestore", { clear = true }),
+        callback = function()
+          if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+            require("persistence").load()
+          end
+        end,
+      })
+    end,
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
+}
