7cc110fb83b62eb8b79af98f068c0951e72df99b
creator54 <hi.creator54@gmail.com>
2026-05-16 13:47:56 +0530

    feat(nvim): add auto-session persistence
    - Add persistence.nvim for session management\n- Configure auto-restore when opening nvim without arguments

--- sync-config.nix
+++ sync-config.nix
@@ -18,6 +18,11 @@ in {
     }
     {
       type = "local";
+      source = "${nixSystemsRoot}/modules/core/configs/common/nvim/plugins/persistence.lua";
+      dest = "~/.config/nvim/lua/plugins/persistence.lua";
+    }
+    {
+      type = "local";
       source = "${nixSystemsRoot}/modules/core/configs/common/aliases";
       dest = "~/.config/aliases";
     }
